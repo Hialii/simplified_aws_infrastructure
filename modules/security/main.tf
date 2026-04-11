b13e685e0e61bd2a43f571cbbfea39a9e64b5a88
hiali <hialidias@gmail.com>
2026-04-10 23:32:35 -0300

    docs: add comments in security module

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -1,14 +1,16 @@
-# Securiry group for EC2
+# Securiry group da EC2
 resource "aws_security_group" "sg-web-ec2" {
    name = "sg-web"
    description = "Security group for web server"
    vpc_id = aws_vpc.main-vpc.id
 } 
 
+# lista de prefixos do cloudfront 
 data "aws_ec2_managed_prefix_list" "cloudfront" {
   name = "com.amazonaws.global.cloudfront.origin-facing"
 }
 
+# regra de entrada para permitir tráfego do Cloufront para a porta 8080 da EC2
 resource "aws_vpc_security_group_ingress_rule" "ec2-cloudfront-ingress-rule" {
    security_group_id = security_group.sg-web-ec2.id
    prefix_list_id = data.aws_ec2_managed_prefix_list.cloudfront.id
@@ -17,6 +19,7 @@ resource "aws_vpc_security_group_ingress_rule" "ec2-cloudfront-ingress-rule" {
    ip_protocol = "tcp"
 }
 
+# regra de entrada para permitir tráfego do meu IP para a porta 22 (ssh) da EC2
 resource "aws_vpc_security_group_ingress_rule" "ec2-ssh-ingress-rule" {
    security_group_id = security_group.sg-web-ec2.id
    cidr_ipv4 =  var.my_ip
@@ -25,21 +28,22 @@ resource "aws_vpc_security_group_ingress_rule" "ec2-ssh-ingress-rule" {
    ip_protocol = "tcp"
 } 
 
-
+# regra de saída para permitir tráfego da EC2 para qualquer destino
 resource "aws_vpc_security_group_egress_rule" "ec2-egress-rule" {
    security_group_id = security_group.sg-web-ec2.id
    cidr_ipv4 = "0.0.0.0/0"
-   ip_protocol = "-1"
+   ip_protocol = "-1" # qualquer protocolo
 }  
 
 
-# Security group for RDS
+# Security group para RDS
 resource "security_group" "sg-rds" {
    name = "sg-rds"
    description = "Security group for RDS"
    vpc_id = aws_vpc.main-vpc.id
 } 
 
+# regra de entrada para permitir tráfego da EC2 para a porta 5432 do RDS
 resource "aws_vpc_security_group_ingress_rule" "rds-ec2-ingress-rule" {
    security_group_id = security_group.sg-rds.id
    from_port = 5432
@@ -48,11 +52,11 @@ resource "aws_vpc_security_group_ingress_rule" "rds-ec2-ingress-rule" {
    referenced_security_group_id = aws_security_group.sg-web-ec2.id
 }
 
-# IAM Role for EC2
+# IAM Role para EC2
 resource "aws_iam_role" "ec2-role" {
    name = ec2-role
    
-   #define quem pode usar a role
+   # Politica de confiança para permitir que a EC2 assuma a role
    assume_role_policy = jsonencode({
       Version = "2012-10-17"
       Statement = [
@@ -66,6 +70,7 @@ resource "aws_iam_role" "ec2-role" {
    })
 }
 
+# Politica com as permissões da EC2
 resource "aws_iam_role_policy" "ec2-policy" {
    name= "ec2-policy"
    role = aws_iam.role.ec2-role.id
@@ -87,6 +92,7 @@ resource "aws_iam_role_policy" "ec2-policy" {
    })
 }
 
+# instance profile para associar a role a EC2
 resource "aws_iam_instance_profile" "ec2-intance-profile" {
    name = "ec2-instance-rofile"
    role = aws_iam_role.ec2-role.name
