a9029a075168fb14aad06745f227d62ddba77119
hiali <hialidias@gmail.com>
2026-04-13 12:31:50 -0300

    feat: add main outputs

---
+++ outputs.tf
@@ -0,0 +1,36 @@
+# Network module outputs
+output "vpc-id" {
+   description = "ID of the VPC"
+   value = module.network.vpc_id
+}
+
+output "public-subnets-id" {
+   description = "IDs of the public subnets"
+   value = module.network.public_subnets_id
+}
+
+output "rds-private-subnets-id" {
+   description = "IDs of the private subnets"
+   value =  module.network.rds_private_subnets_id
+}
+
+output "internet-gateway-id" {
+   description = "ID of the Internet Gateway"
+   value =  module.network.internet_gateway_id
+}
+
+# Security module outputs
+output "sg_web_ec2_id" {
+  description = "Security Group ID for EC2 instances"
+  value       = module.security.sg_web_ec2_id
+}
+
+output "sg_rds_id" {
+  description = "Security Group ID for RDS instances"
+  value       = module.security.sg_rds_id
+}
+
+output "ec2_iam_profile_name" {
+  description = "Instance Profile Name for EC2 instances"
+  value       = module.security.ec2_iam_profile_name
+}
